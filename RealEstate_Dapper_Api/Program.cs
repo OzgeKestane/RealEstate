af5ca973d67a4c011ad48e54fc879145098901ad
Özge Kestane <ozgekestane97@gmail.com>
2024-09-22 22:13:52 +0300

    Service Api added

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,6 +1,7 @@
 using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Repositories.CategoryRepositories;
 using RealEstate_Dapper_Api.Repositories.ProductRepositories;
+using RealEstate_Dapper_Api.Repositories.ServiceRepositories;
 using RealEstate_Dapper_Api.Repositories.WhoWeAreRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddTransient<Context>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
