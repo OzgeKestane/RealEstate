9b072cecece88b0821529e98add66d8fc90a47b9
Özge Kestane <ozgekestane97@gmail.com>
2024-09-29 02:40:56 +0300

    AdminPanel Category Dropdawn added

--- RealEstate_Dapper_UI/Dtos/ProductDtos/CreateProductDto.cs
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/CreateProductDto.cs
@@ -6,7 +6,7 @@
         public decimal price { get; set; }
         public string city { get; set; }
         public string district { get; set; }
-        public string categoryName { get; set; }
+        public string categoryId { get; set; }
         public string coverimage { get; set; }
         public string description { get; set; }
         public string type { get; set; }
